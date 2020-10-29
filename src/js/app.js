d022f4ae13cd12986b77e060d5a2596902c190ca
Andrew Luchuk <spaxumof@gmail.com>
2020-10-28 22:18:40 -0400

    basic app functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,42 @@
 let converter = new Vue({
     el: '#converter',
     data: {
-        rates: {}
+        rates: {},
+        base: '',
+        date: '',
+        amountValue: null,
+    },
+    methods: {
+        calculateValue: function() {
+            let amountElement = document.getElementById('amount');
+            if (amountElement.value !== '') {
+                let amount = amountElement.value;
+                let base = getBaseRate();
+                let target = getTargetRate();
+                this.amountValue = convertToTarget(base, target, amount).toFixed(2);
+            }
+        }
     }
+});
+
+function convertToTarget(baseRate, targetRate, amount) {
+    return (amount / baseRate) * targetRate;
+}
+
+function getBaseRate() {
+    let base = document.getElementById("base-currency");
+    let baseName = base.value;
+    return converter.rates[baseName];
+}
+
+function getTargetRate() {
+    let target = document.getElementById('target-currency');
+    let targetName = target.value;
+    return converter.rates[targetName];
+}
+
+fetch('https://api.exchangeratesapi.io/latest').then(res => res.json()).then(json => {
+    converter.rates = json.rates;
+    converter.base = json.base;
+    converter.date = json.date;
 });
